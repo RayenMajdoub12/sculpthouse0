498bc78615577ab6c6843401689dff903b17ed33
daliabid61 <75262113+daliabid61@users.noreply.github.com>
2021-01-04 00:06:13 +0100

    edit update

--- sculpthouse_back/editProduct.php
+++ sculpthouse_back/editProduct.php
@@ -1,5 +1,8 @@
+
+
+<?php require 'header_sidebar.php';?>
 <?php  
-session_start();
+
 
 
 if(!isset($_GET['id']))
@@ -47,7 +50,7 @@ $image = $_POST['image'];
 
     $obj = $mysqli->query("UPDATE `tbl_product` SET `name` = '" . $_POST['name'] ."', `price`='". $_POST["price"] ."', `quantite`='".$_POST["quantite"]."' WHERE `id` = $productId;") or die("0");
 
-    header('Location: basic_table.php');
+    header('Location: product_order.php');
 
 
 }
@@ -64,14 +67,14 @@ $image = $_POST['image'];
 
 
 
-<?require 'header_sidebar.php';?>
+
     <!-- **********************************************************************************************************************************************************
         MAIN CONTENT
         *********************************************************************************************************************************************************** -->
     <!--main content start-->
     <section id="main-content">
       <section class="wrapper">
-        <h3><i class="fa fa-angle-right"></i> Form Validation</h3>
+        
         <!-- BASIC FORM VALIDATION -->
         
                 
@@ -127,73 +130,7 @@ $image = $_POST['image'];
           <!-- /col-lg-12 -->
         </div>
         <!-- /row -->
-        <div class="row mt">
-          <div class="col-lg-12">
-            <h4><i class="fa fa-angle-right"></i> Advanced Form Validations</h4>
-            <div class="form-panel">
-              <div class="form">
-                <form class="cmxform form-horizontal style-form" id="signupForm" method="get" action="">
-                  <div class="form-group ">
-                    <label for="firstname" class="control-label col-lg-2">Firstname</label>
-                    <div class="col-lg-10">
-                      <input class=" form-control" id="firstname" name="firstname" type="text" />
-                    </div>
-                  </div>
-                  <div class="form-group ">
-                    <label for="lastname" class="control-label col-lg-2">Lastname</label>
-                    <div class="col-lg-10">
-                      <input class=" form-control" id="lastname" name="lastname" type="text" />
-                    </div>
-                  </div>
-                  <div class="form-group ">
-                    <label for="username" class="control-label col-lg-2">Username</label>
-                    <div class="col-lg-10">
-                      <input class="form-control " id="username" name="username" type="text" />
-                    </div>
-                  </div>
-                  <div class="form-group ">
-                    <label for="password" class="control-label col-lg-2">Password</label>
-                    <div class="col-lg-10">
-                      <input class="form-control " id="password" name="password" type="password" />
-                    </div>
-                  </div>
-                  <div class="form-group ">
-                    <label for="confirm_password" class="control-label col-lg-2">Confirm Password</label>
-                    <div class="col-lg-10">
-                      <input class="form-control " id="confirm_password" name="confirm_password" type="password" />
-                    </div>
-                  </div>
-                  <div class="form-group ">
-                    <label for="email" class="control-label col-lg-2">Email</label>
-                    <div class="col-lg-10">
-                      <input class="form-control " id="email" name="email" type="email" />
-                    </div>
-                  </div>
-                  <div class="form-group ">
-                    <label for="agree" class="control-label col-lg-2 col-sm-3">Agree to Our Policy</label>
-                    <div class="col-lg-10 col-sm-9">
-                      <input type="checkbox" style="width: 20px" class="checkbox form-control" id="agree" name="agree" />
-                    </div>
-                  </div>
-                  <div class="form-group ">
-                    <label for="newsletter" class="control-label col-lg-2 col-sm-3">Receive the Newsletter</label>
-                    <div class="col-lg-10 col-sm-9">
-                      <input type="checkbox" style="width: 20px" class="checkbox form-control" id="newsletter" name="newsletter" />
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <div class="col-lg-offset-2 col-lg-10">
-                      <button class="btn btn-theme" type="submit">Save</button>
-                      <button class="btn btn-theme04" type="button">Cancel</button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </div>
-            <!-- /form-panel -->
-          </div>
-          <!-- /col-lg-12 -->
-        </div>
+        
         <!-- /row -->
       </section>
       <!-- /wrapper -->
